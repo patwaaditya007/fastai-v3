0539fae8a1aab63bff2729509c6d3483acb46f82
patwaaditya007 <65835334+patwaaditya007@users.noreply.github.com>
2020-05-24 05:44:07 +0530

    git commit server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=144Yatl0TV_oER8u009EjaWMARfvZIw1y'
+export_file_name = 'cnn.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['kanye','wiz']
 path = Path(__file__).parent
 
 app = Starlette()
